9cb06071abdb9e183e661be7a3039162c41f0f54
Lee Surprenant <lmsurpre@us.ibm.com>
2021-03-18 10:01:03 -0400

    Introduce separate bulkdata config params for file export
    Previously, the file-based export would use the COS properties for
    checkpointing. This is a bit confusing and, additionally, I think we'd
    want a different default for "when to start writing" (in COS there are
    constraints on the size and count of the parts, while writing to local
    disk has different concerns).
    
    Signed-off-by: Lee Surprenant <lmsurpre@us.ibm.com>

--- fhir-bulkdata-webapp/src/main/java/com/ibm/fhir/bulkdata/jbatch/export/checkpoint/ExportCheckpointAlgorithm.java
+++ fhir-bulkdata-webapp/src/main/java/com/ibm/fhir/bulkdata/jbatch/export/checkpoint/ExportCheckpointAlgorithm.java
@@ -20,6 +20,7 @@ import javax.inject.Inject;
 
 import com.ibm.fhir.bulkdata.jbatch.context.BatchContextAdapter;
 import com.ibm.fhir.bulkdata.jbatch.export.data.ExportTransientUserData;
+import com.ibm.fhir.operation.bulkdata.config.ConfigurationAdapter;
 import com.ibm.fhir.operation.bulkdata.config.ConfigurationFactory;
 import com.ibm.fhir.operation.bulkdata.model.type.BulkDataContext;
 
@@ -39,6 +40,8 @@ public class ExportCheckpointAlgorithm implements CheckpointAlgorithm {
 
     Boolean isFileExport;
 
+    ConfigurationAdapter adapter;
+
     public ExportCheckpointAlgorithm() {
         // No Operation
     }
@@ -56,6 +59,10 @@ public class ExportCheckpointAlgorithm implements CheckpointAlgorithm {
             BatchContextAdapter contextAdapter = new BatchContextAdapter(jobExecution.getJobParameters());
             BulkDataContext ctx = contextAdapter.getStepContextForSystemChunkWriter();
             isFileExport = "file".equals(ConfigurationFactory.getInstance().getStorageProviderType(ctx.getSource()));
+        }
+
+        if (adapter == null) {
+            adapter = ConfigurationFactory.getInstance();
         }
 
         if (logger.isLoggable(Level.FINE)) {
@@ -97,16 +104,19 @@ public class ExportCheckpointAlgorithm implements CheckpointAlgorithm {
             return false;
         }
 
-        long cosFileMaxResources = ConfigurationFactory.getInstance().getCoreCosObjectResourceCountThreshold();
-        long cosFileThresholdSize = ConfigurationFactory.getInstance().getCoreCosObjectSizeThreshold();
-        long cosMultiPartMinSize = ConfigurationFactory.getInstance().getCoreCosPartUploadTriggerSize();
+        long resourceCountThreshold = isFileExport ? adapter.getCoreFileResourceCountThreshold()
+                : adapter.getCoreCosObjectResourceCountThreshold();
+        long sizeThreshold = isFileExport ? adapter.getCoreFileSizeThreshold()
+                : adapter.getCoreCosObjectSizeThreshold();
+        long writeTrigger = isFileExport ? adapter.getCoreFileWriteTriggerSize()
+                : adapter.getCoreCosPartUploadTriggerSize();
 
         // Set to true if we have enough bytes to write a part
-        boolean readyToWrite = isFileExport || (chunkData.getBufferStream().size() > cosMultiPartMinSize);
+        boolean readyToWrite = chunkData.getBufferStream().size() >= writeTrigger;
 
         // Check if we should finish writing the current object/file
-        boolean overFileSizeThreshold = cosFileThresholdSize != 0 && chunkData.getBufferStream().size() >= cosFileThresholdSize;
-        boolean overMaxResourceCountThreshold = cosFileMaxResources != 0 && chunkData.getCurrentUploadResourceNum() >= cosFileMaxResources;
+        boolean overFileSizeThreshold = sizeThreshold != 0 && chunkData.getBufferStream().size() >= sizeThreshold;
+        boolean overMaxResourceCountThreshold = resourceCountThreshold != 0 && chunkData.getCurrentUploadResourceNum() >= resourceCountThreshold;
         boolean end = chunkData.getPageNum() >= chunkData.getLastPageNum();
         chunkData.setFinishCurrentUpload(overFileSizeThreshold || overMaxResourceCountThreshold || end);
 
